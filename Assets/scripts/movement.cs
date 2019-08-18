3974beb332eb20cfb9303be9a38275532be0fb91
Len1n <1eav@inbox.ru>
2019-08-18 11:39:15 +0300

    movement impl.

--- Assets/scripts/movement.cs
+++ Assets/scripts/movement.cs
@@ -3,19 +3,55 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class movement : MonoBehaviour
-    public GameObject obj;
-public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+
 {
-    // Start is called before the first frame update
+    public GameObject obj;
+    public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+
+
     void Start()
     {
-        
+
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-    if (Input.GetKey(KeyCode.UpArrow))
-        obj.transform.Translate(Vector3.forward  moveSpeed  Time.deltaTime);
+        Move();
+        EvenOut();
+    }
+    
+
+    void Move()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            obj.transform.Translate(Vector3.back * moveSpeed * Time.deltaTime);
+
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            obj.transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            obj.transform.Translate(Vector3.left * moveSpeed * Time.deltaTime);
+
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            obj.transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+
+    }
+
+    void EvenOut()
+    {
+        if (Input.GetMouseButtonDown(2))
+        {
+            // obj.rotation.transform.y = 0;
+            //obj.rotation.transform.x = 0;
+            //obj.rotation.transform.z = 0;
+            obj.transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
+       
+    }
+    
 }
-}
+
