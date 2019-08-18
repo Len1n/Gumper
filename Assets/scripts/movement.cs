7bffca3645e33d0504048c379841ad9c60cf2fcc
Len1n <1eav@inbox.ru>
2019-08-18 10:51:52 +0300

    first commit

---
+++ Assets/scripts/movement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+    public GameObject obj;
+public float range = 5f, moveSpeed = 3f, turnSpeed = 40f;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    if (Input.GetKey(KeyCode.UpArrow))
+        obj.transform.Translate(Vector3.forward  moveSpeed  Time.deltaTime);
+}
+}
